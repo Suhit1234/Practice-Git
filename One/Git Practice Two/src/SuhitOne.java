751f05e6397cc3b4ba76ae0cdeeb1740e6dbe614
Suhit <suhit@Suhit-MacBook-Pro.local>
2018-01-13 13:06:47 +0530

    Practice

---
+++ One/Git Practice Two/src/SuhitOne.java
@@ -0,0 +1,17 @@
+
+public class SuhitOne {
+	
+	public void display(){
+		System.out.println("Suhit1");
+		System.out.println("Suhit2");
+		System.out.println("Suhit3");
+		System.out.println("Suhit4");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SuhitOne One = new SuhitOne();
+		One.display();
+	}
+
+}
